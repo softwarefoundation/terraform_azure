18815df607f67792f6045924a00e86a4ebf0947b
Dherkyan Ribeiro <dherkyan@gmail.com>
2022-03-04 06:17:24 -0300

    60. [Hands-On] Usando Concat e Length

--- funcoes_expressoes_loops/main.tf
+++ funcoes_expressoes_loops/main.tf
@@ -12,3 +12,19 @@ resource "azurerm_resource_group" "grupo-recurso" {
     tecnologia = title("terraform treianmento")
   }
 }
+
+variable "vnet_ips" {
+  type = list
+  default = ["10.0.0.0/16"]
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  address_space       = concat(var.vnet_ips, ["192.168.0.0/16"])
+  location            = "brazilsouth"
+  name                = "vnet_treinamento_azure"
+  resource_group_name = "${azurerm_resource_group.grupo-recurso.name}"
+}
+
+output "vnet_lista_ips" {
+  value = length("${azurerm_virtual_network.vnet.address_space}")
+}
