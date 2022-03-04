634683d41392375d8f2cbc38ab2a1837e855dbfe
Dherkyan Ribeiro <dherkyan@gmail.com>
2022-03-04 06:06:09 -0300

    59. [Hands-On] Usando String Functions

--- funcoes_expressoes_loops/main.tf
+++ funcoes_expressoes_loops/main.tf
@@ -6,6 +6,9 @@ resource "azurerm_resource_group" "grupo-recurso" {
   location = "brazilsouth"
   name     = "rg-terraform-modulo-5"
   tags = {
-    data = formatdate("DD MMM YYYY hh:mm ZZZ", timestamp())
+    data = formatdate("DD MMM YYYY hh:mm ZZZ", timestamp()),
+    ambiente = lower("Homologacao")
+    autor = upper("Yan")
+    tecnologia = title("terraform treianmento")
   }
 }
